bc5a9f9fa5fd799a09e5e285d6d50baee6ce9259
jaiden-lee <jaidenwlee@gmail.com>
2025-07-15 23:16:43 -0400

    refactor to use link component instead of useRouter

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,14 +1,15 @@
-import { useRouter, Tabs } from "expo-router";
+import { Tabs, Link } from "expo-router";
 import { TouchableOpacity, Text } from "react-native";
 
 export default function TabsLayout() {
-    const router = useRouter();
 
     return (
         <>
-            <TouchableOpacity onPress={() => router.push("/camera")} style={{ position: "absolute", bottom: 150, right: 40, zIndex: 1000, backgroundColor: "white", borderRadius: 100, padding: 10 }}>
-                <Text>Camera</Text>
-            </TouchableOpacity>
+            <Link href="/camera" asChild>
+                <TouchableOpacity style={{ position: "absolute", bottom: 150, right: 40, zIndex: 1000, backgroundColor: "white", borderRadius: 100, padding: 10 }}>
+                    <Text>Camera</Text>
+                </TouchableOpacity>
+            </Link>
             <Tabs screenOptions={{ headerShown: false }} >
                 <Tabs.Screen name="home" options={{ title: "Home" }} />
                 <Tabs.Screen name="progress" options={{ title: "Progress" }} />
