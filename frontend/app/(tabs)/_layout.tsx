355d50882ef7e30db2e7a8f39fd678fd4b990210
jaiden-lee <jaidenwlee@gmail.com>
2025-07-20 17:55:31 -0400

    floating action button for camera

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -5,22 +5,21 @@ import { Link } from "expo-router";
 
 export default function TabsLayout() {
     return (
-        <View className="flex-1">
-            <Tabs
-                screenOptions={{
-                    headerShown: false,
-                    tabBarActiveTintColor: "black",
-                    tabBarLabelStyle: { fontFamily: "Poppins-SemiBold" },
-                }}>
-                <Tabs.Screen name="home" options={{ title: "Home", tabBarIcon: ({ color }) => <Feather name="home" color={color} size={23} /> }} />
-                <Tabs.Screen name="progress" options={{ title: "Progress", tabBarIcon: ({ color }) => <Feather name="bar-chart" color={color} size={23} /> }} />
-                <Tabs.Screen name="settings" options={{ title: "Settings", tabBarIcon: ({ color }) => <Feather name="settings" color={color} size={23} /> }} />
-            </Tabs>
-            <Link href="/camera" asChild>
-                <TouchableOpacity className="absolute bottom-10 right-10 bg-black p-4 rounded-full">
-                    <Feather name="camera" size={23} color="white" />
-                </TouchableOpacity>
-            </Link>
-        </View>
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: "black",
+                tabBarLabelStyle: { fontFamily: "Poppins-SemiBold" },
+            }}>
+            <Tabs.Screen name="home" options={{ title: "Home", tabBarIcon: ({ color }) => <Feather name="home" color={color} size={23} /> }} />
+            <Tabs.Screen name="progress" options={{ title: "Progress", tabBarIcon: ({ color }) => <Feather name="bar-chart" color={color} size={23} /> }} />
+            <Tabs.Screen
+                name="settings"
+                options={{
+                    title: "Settings",
+                    tabBarIcon: ({ color }) => <Feather name="settings" color={color} size={23} />,
+                }}
+            />
+        </Tabs>
     );
 }
--- frontend/src/components/elements/tab-view.tsx
+++ frontend/src/components/elements/tab-view.tsx
@@ -1,6 +1,9 @@
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import HeaderText from "@/src/components/elements/header-text";
 import { View } from "react-native";
+import { Link } from "expo-router";
+import { TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 export default function TabView({ children, className = "", headerText }: { children: React.ReactNode, className?: string, headerText: string }) {
     const insets = useSafeAreaInsets();
@@ -11,6 +14,11 @@ export default function TabView({ children, className = "", headerText }: { chil
                 <HeaderText className="text-3xl">{headerText}</HeaderText>
                 {children}
             </View>
+            <Link href="/camera" asChild>
+                <TouchableOpacity className="absolute bottom-4 right-4 bg-black p-5 rounded-full">
+                    <Feather name="plus" size={24} color="white" />
+                </TouchableOpacity>
+            </Link>
         </View>
     );
 }
