53c76a279b2b7f1f7b8f8461013db6697dd6822e
jaiden-lee <jaidenwlee@gmail.com>
2025-07-20 16:35:31 -0400

    fonts work + global impors

--- frontend/app/(tabs)/home.tsx
+++ frontend/app/(tabs)/home.tsx
@@ -1,13 +1,11 @@
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import HeaderText from "../../src/components/elements/header-text";
+import HeaderText from "@/src/components/elements/header-text";
 
 export default function Home() {
     return (
-        <SafeAreaView>
-            <View>
-                <HeaderText>Home</HeaderText>
-            </View>
+        <SafeAreaView className="flex-1 p-4">
+            <HeaderText className="text-3xl">Home</HeaderText>
         </SafeAreaView>
     );
 }
--- frontend/src/components/elements/header-text.tsx
+++ frontend/src/components/elements/header-text.tsx
@@ -1,6 +1,6 @@
 import { Text } from "react-native";
 
-export default function HeaderText({ children, className = "" }: { children: React.ReactNode, className?: string }) {
+export default function HeaderText({ children, className="" }: { children: React.ReactNode, className?: string }) {
     return (
         <Text className={`font-poppins-semibold ${className}`}>
             {children}
