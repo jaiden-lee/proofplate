c0a736115677a5aa173a0b24e177d2768b5909fd
jaiden-lee <jaidenwlee@gmail.com>
2025-07-21 23:00:45 -0400

    redirect logic to onboarding or main

---
+++ frontend/app/index.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { Redirect, RelativePathString } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export default function Index() {
+    const [initialRoute, setInitialRoute] = useState<RelativePathString | null>(null);
+
+    useEffect(() => {
+        const checkOnboarding = async () => {
+            const onboardingCompleted = await AsyncStorage.getItem("onboardingCompleted");
+            if (onboardingCompleted) {
+                setInitialRoute("./(main)/(tabs)/home");
+            } else {
+                setInitialRoute("./(onboarding)/index");
+            }
+        }
+
+        checkOnboarding();
+    }, []);
+
+    if (!initialRoute) {
+        return null;
+    }
+
+    return <Redirect href={initialRoute} />;
+}
